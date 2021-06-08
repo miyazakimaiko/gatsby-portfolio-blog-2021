a5e143ab82af737f7bdef35e27dcee6db719595b
miyazakimaiko <myzkmik19922@gmail.com>
2021-06-08 13:24:08 +0100

    Added gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,5 +130,16 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+          resolve: `gatsby-remark-vscode`,
+          options: {
+            theme: 'Abyss' // Or install your favorite theme from GitHub
+          }
+        }]
+      }
+    },
   ],
 }
